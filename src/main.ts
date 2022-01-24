f7ec41d248b8c5beb92053278d061f7cf6ca5454
MicKing110 <18710009726@sina.cn>
2022-01-24 15:35:50 +0800

    add

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,10 @@ import router from './router'
 import { store, key } from './store'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import VueI18n from "./lang/index";
 // 加载全局样式
 import './style/index.scss'
 createApp(App)
   .use(router)
-  .use(VueI18n)
   .use(store, key)
   .use(ElementPlus)
   .mount('#app')
